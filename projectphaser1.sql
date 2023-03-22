204cd6ecbc0d602b4c97e3f9cc4250dcce726b5a
Micah Too <111716341+mktoon@users.noreply.github.com>
2023-03-21 19:42:32 -0600

    phase1 completed

--- projectphaser1.sql
+++ projectphaser1.sql
@@ -1,28 +1,35 @@
-CREATE DATABASE Hospitalmanagement;
+CREATE DATABASE HospitalDB;
 CREATE TABLE physician(
         physicianID int NOT NULL,
-        name varchar(40) Not NULL,
-        `position` varchar(40),
+        name varchar(40) NOT NULL,
+        position varchar(40) NOT NULL CHECK(position IN('Intern','Surgeon','Senior','Chief Of Medicine','Resident','Psychiatrist')),
         SSN int,
         PRIMARY KEY (physicianID)
 );
+
 CREATE TABLE department(
         deptID int,
-        name varchar(40),
-        headID int,
-        FOREIGN KEY(headID) REFERENCES physician(physicianID)        
+        name varchar(40) NOT NULL CHECK (name IN ('General Medicine', 'Surgery', 'Psychiatry')),
+        headID int NOT NULL, 
+        FOREIGN KEY(headID) REFERENCES physician(physicianID),
+        PRIMARY KEY(deptID)
 );
+
 CREATE TABLE affiliatedwith(
         physicianID int,
         departmentID int,
         FOREIGN KEY (physicianID) REFERENCES physician(physicianID),
-        FOREIGN KEY (departmentID) REFERENCES department(deptID)
+        FOREIGN KEY (departmentID) REFERENCES department(deptID),
+        PRIMARY KEY(physicianID, departmentID)
 );
+
 CREATE TABLE `procedure`(
         procID int,
         name varchar(40),
-        cost real
+        cost real,
+        PRIMARY KEY(procID)
 );
+
 CREATE TABLE patient(
         patientID int,
         SSN int,
@@ -32,18 +39,23 @@ CREATE TABLE patient(
         phone varchar(16),
         insuranceNumber int,
         primaryphysicianID int,
-        foreign key (primaryphysicianID) references physician(physicianID)
+        FOREIGN KEY (primaryphysicianID) REFERENCES physician(physicianID),
+        PRIMARY KEY(patientID)
 );
+
 CREATE TABLE nurse(
         nurseID int,
         name varchar(40),
-        `position` varchar(40),
-        SSN int
+        position varchar(40) NOT NULL CHECK(position IN('Head Nurse', 'Nurse')),
+        SSN int,
+        PRIMARY KEY(nurseID)
 );
+
 CREATE TABLE medication(
-         medID int,
+         medID int NOT NULL PRIMARY KEY,
          name varchar(40)
 );
+
 CREATE TABLE prescribes(
         physicianID int,
         patientID int,
@@ -51,12 +63,17 @@ CREATE TABLE prescribes(
         prescribedDate date,
         dose varchar(40),
         FOREIGN KEY (physicianID) REFERENCES physician(physicianID),
-        FOREIGN KEY (patientID) REFERENCES patient(patientID)
+        FOREIGN KEY (patientID) REFERENCES patient(patientID),
+        FOREIGN KEY (medicationID) REFERENCES medication(medID),
+        PRIMARY KEY(physicianID, patientID, medicationID)
 );
+
 CREATE TABLE room(
         roomID int,
-        roomType varchar(40)        
+        roomType varchar(40) NOT NULL CHECK (roomType IN ('Single', 'Double')),
+        PRIMARY KEY(roomID)
 );
+
 CREATE TABLE stay(
         stayID int,
         patientID int,
@@ -64,8 +81,10 @@ CREATE TABLE stay(
         startDate date,
         endDate date,
         FOREIGN KEY (patientID) REFERENCES patient(patientID),
-        FOREIGN KEY (roomID) REFERENCES room(roomID)
+	    FOREIGN KEY (roomID) REFERENCES room(roomID),
+        PRIMARY KEY(stayID)
 );
+
 CREATE TABLE undergoes(
         patientID int,
         procedureID int,
@@ -74,18 +93,21 @@ CREATE TABLE undergoes(
         physicianID int,
         nurseID int,
         FOREIGN KEY (patientID) REFERENCES patient(patientID),
-        FOREIGN KEY (procID) REFERENCES `procedure`(patientID),
+        FOREIGN KEY (procedureID) REFERENCES `procedure`(procID),
         FOREIGN KEY (stayID) REFERENCES stay(stayID),
-        FOREIGN KEY (physicianID) REFERENCES phycisian(physicianID),
-        FOREIGN KEY (nurseID) REFERENCES nurse(nurseID)
-        
+        FOREIGN KEY (physicianID) REFERENCES physician(physicianID),
+        FOREIGN KEY (nurseID) REFERENCES nurse(nurseID),
+        PRIMARY KEY(patientID, procedureID, stayID, physicianID, nurseID)
 );
+
 CREATE TABLE on_call_nurse(
         nurseID int,
         startDate date,
         endDate date,
-        FOREIGN KEY (nurseID) REFERENCES nurse(nurseID)
+        FOREIGN KEY (nurseID) REFERENCES nurse(nurseID),
+        PRIMARY KEY(nurseID)
 );
+
 CREATE TABLE appointment(
         appID int,
         patientID int,
@@ -95,6 +117,6 @@ CREATE TABLE appointment(
         endDate datetime,
         FOREIGN KEY (patientID) REFERENCES patient(patientID),
         FOREIGN KEY (nurseID) REFERENCES nurse(nurseID),
-        FOREIGN KEY (physicianID) REFERENCES physician(physicianID)        
+        FOREIGN KEY (physicianID) REFERENCES physician(physicianID),
+        PRIMARY KEY(appID)
 );
-INSERT INTO EMPLOYEE VALUES (0001, 'Dave', 'Sales');
